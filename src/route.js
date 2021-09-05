4951f380fad0e9610bfe7dedd7b73149f1a333ec
habaumugisha1 <habajeunes2.com>
2021-09-05 08:10:27 +0200

    correct

--- src/route.js
+++ src/route.js
@@ -4,7 +4,7 @@ import { randomWords } from "./controllers/generateWords";
 import {  leaderBoards} from "./controllers/leaderboard";
 
 const router = express.Router()
-router.get('/', (req, res) => res.status(200).json({ status:200, message:'Welcome to store Management system'}));
+router.get('/', (req, res) => res.status(200).json({ status:200, message:'Welcome to Word Race Game'}));
 router.post('/newgame', newGame);
 router.get('/words', randomWords);
 router.get('/leaderBoard', leaderBoards);
